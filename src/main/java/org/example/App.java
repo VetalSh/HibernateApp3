9eef851fd69c505158f83d7a11559944a976bcf6
vitaliiSh <vitalii.shkar@gmail.com>
2023-09-07 13:56:43 +0300

    cascading type persist

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,16 +1,21 @@
 package org.example;
 
+import org.example.model.Item;
 import org.example.model.Person;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+
+import java.util.ArrayList;
+import java.util.Collections;
 
 /**
  * This is small app which demonstrate using Hibernate
  */
 public class App {
     public static void main( String[] args ) {
-        Configuration configuration = new Configuration().addAnnotatedClass(Person.class);
+        Configuration configuration = new Configuration().addAnnotatedClass(Person.class)
+            .addAnnotatedClass(Item.class);
 
         SessionFactory sessionFactory = configuration.buildSessionFactory();
         Session session = sessionFactory.getCurrentSession();
@@ -18,6 +23,11 @@ public class App {
         try {
             session.beginTransaction();
 
+            Person person = new Person("Test cascading", 30);
+            Item item = new Item("Test cascading item", person);
+            person.setItems(new ArrayList<>(Collections.singletonList(item)));
+
+            session.persist(person);
 
             session.getTransaction().commit();
         } finally {
