b484a2318dfed4db3d078ad9c910a54374b62608
vitaliiSh <vitalii.shkar@gmail.com>
2023-09-07 14:05:39 +0300

    cascading type save_update

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -27,7 +27,7 @@ public class App {
             Item item = new Item("Test cascading item", person);
             person.setItems(new ArrayList<>(Collections.singletonList(item)));
 
-            session.persist(person);
+            session.save(person);
 
             session.getTransaction().commit();
         } finally {
